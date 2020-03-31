211f7c41ec9c64ce75d07e5e595ef88c8d342a57
Jonathan <jsul427@aucklanduni.ac.nz>
2020-03-31 17:39:22 +1300

    Set up card react component

--- game/src/App.js
+++ game/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from './cards/Card';
 
 function App() {
+  var cardlist = [];
+  for(let i = 13; i > 0; i--){
+    cardlist.push("c"+i);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {cardlist.map((x,i) => <Card card={x} position={i}/>)}
     </div>
   );
 }
