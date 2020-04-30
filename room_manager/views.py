cc08c85790fd3b88a3428c93f300239268a62422
Stephen Joe <28199363+stkjoe@users.noreply.github.com>
2020-05-01 00:22:28 +1200

    Remove serializers from the project
    Not needed as the JSON data taken from the website can be
    justified to not be complex enough to warrant it.

--- room_manager/views.py
+++ room_manager/views.py
@@ -12,29 +12,30 @@ class CreateRoomView(APIView):
 
     # Create room
     def post(self, request):
-        serializer = RoomSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
+        game = request.data.get("game")
+        max_players = request.data.get("max_players")
+        while True:
             # Generate a new random code and see if it exists already. If not, break loop
-            while True:
-                room_code = ''.join(random.choice(string.ascii_uppercase) for _ in range(5))
-                if not Room.objects.filter(code=room_code).first():
-                    break
-            serializer.save(code=room_code)
-            content = {'success': room_code}
-            return Response(content)
+            code = ''.join(random.choice(string.ascii_uppercase) for _ in range(5))
+            if not Room.objects.filter(code=code).first():
+                break
+        room = Room(game=game, max_players=max_players, code=code)
+        room.save()
+        content = {'success': room_code}
+        return Response(content)
 
 class JoinRoomView(APIView):
 
     # Join room
-    def get(self, request, room_code):
-
+    def get(self, request, code):
+        
+        room = Room.objects.get(code=code)
         # See if the current room exists
-        if not Room.objects.filter(code=room_code).first():
+        if not room:
             content = {'error': 'Room does not exist.'}
             return Response(content)
 
         # See if the current room is full
-        room = Room.objects.get(code=room_code)
         if room.players == room.max_players:
             content = {'error': 'Room is full.'}
             return Response(content)
@@ -45,10 +46,10 @@ class JoinRoomView(APIView):
             return Response(content)
 
         # Successful join, increment the player count by 1
-        Room.objects.filter(code=room_code).update(players=F('players')+1)
+        Room.objects.get(code=code).update(players=F('players')+1)
 
         # Create the game
-        game = Game(code=room_code, max_players=room.max_players)
+        game = Game(code=code)
         game.save()
 
         # Returns the room code
